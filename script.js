6b60c7b7239c9d7ed2b9f7592b051f61a9bd0f92
Simone Muscas <smuscas.hcie-m2020@fh-salzburg.ac.at>
2022-06-05 22:06:03 +0200

    working on the winning function

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
 
 const Player = (sign) => {
-    this.sign = sign;
+    var obj = {}
+    obj.sign = sign;
+    obj.fields = [];
+    return obj;
 }
+
+
 
 const gameboard = (function () {
     
     const buttons = document.querySelectorAll('.field')
-    
-    let fields = ["","x","", "","o","", "o","",""]
+    console.log(buttons)
+    let fields = ["","","", "","","", "","",""]
+    buttons.forEach( field => { field.addEventListener('click', () => {gameFlow.playRound(field.id)})})
+    const fillField = (sign, id) => {
+        fields[id - 1] = sign;
+        display();
+    }
+
     const display = () => {
         console.log("it works")
         for(let x = 0; x < fields.length; x++) {
@@ -21,7 +32,7 @@ const gameboard = (function () {
             }
         }
     }
-    return {display, fields}
+    return {display, fields, fillField}
     })()
 
 gameboard.display()
@@ -29,16 +40,40 @@ const player = (function () {})();
 
 const gameFlow = (() => {
 
-    const player1 = Player("X");
-    const player2 = Player("O");
-    let round = 1;
+    const player1 = Player("x");
+    const player2 = Player('o');
+    const roundCounter = document.querySelector('.round-counter')
+    let round = 0;
+    const checkWinner = (player) => {
+        const winningPatterns = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+        for (let i = 0; i < winningPatterns.length; i++) {
+            if (winningPatterns[i].every(element => {
+            player.fields.includes(element) })) {
+            return `PLAYER WON!`}
+        }
+
+        if (player.fields)
+        if (round == 9) {
+            return "DRAW!"
+        }
+    }
+    roundCounter.textContent = `Round ${round}`
     let gameIsOver = false;
+    const playRound = (id) => {
 
+        //if field not empty -> return
+        round++
+        roundCounter.textContent = `Round ${round}`
+        var player = (round % 2 == 1) ? player1 : player2;
+        player.fields.push(id);
+        gameboard.fillField(player.sign, id);
+        checkWinner(player)
 
-    const playRound()
-    const getCurrentPlayerSign = () => {
-        return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
-    };
+    }
+
+    return {playRound}
+     // round % 2 === 1 ? player.getSign() : player.getSign();
 })();
 
 //and you’re probably going to want an object to control the flow of the game itself
